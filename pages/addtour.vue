bbf22108a009ff23c432931815bc957e19187525
OB02CGTH <sornsiri1208@gmail.com>
2023-03-10 15:37:03 +0700

    UpdateTour

--- pages/addtour.vue
+++ pages/addtour.vue
@@ -1,17 +1,17 @@
 <template>
   <div style="
-              border-radius: 1rem;
-              box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
-                rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
-              margin: 1rem;
-          ">
+      border-radius: 1rem;
+      box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
+      rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
+      margin: 1rem;
+      ">
     <v-row style="margin: 1rem">
       <v-col style="
-              border-radius: 1rem;
-              box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
-                rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
-              margin: 1rem;
-          ">
+        border-radius: 1rem;
+        box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
+        rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
+        margin: 1rem;
+        ">
         <h1
           class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white">
           ข้อมูลทัวร์</h1>
@@ -35,48 +35,53 @@
         </div>
         <div class="mb-6">
           <label for="base-input"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">โปรแกรมทัวร์</label>
-          <input type="text" id="small-input"
-            class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-        </div>
-        <div class="mb-6">
-          <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">จำนวนวัน</label>
-          <input type="text" id="small-input"
-            class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-        </div>
-        <div class="mb-6">
-          <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">จำนวนคืน</label>
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ชื่อโปรแกรมทัวร์</label>
           <input type="text" id="small-input"
             class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
         </div>
         <div class="flex">
-          <div class="relative max-w-sm">
-            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
-                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                  clip-rule="evenodd"></path>
-              </svg>
-            </div>
-            <input datepicker type="text"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="วันที่ไป" />
+          <div class="mb-6">
+            <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">จำนวนวัน</label>
+            <input type="text" id="base-input"
+              class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
           </div>
-          <div class="relative max-w-sm">
-            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
-                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                  clip-rule="evenodd"></path>
-              </svg>
-            </div>
-            <input datepicker type="text"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="วันที่กลับ" />
+          <div class="mb-6">
+            <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">จำนวนคืน</label>
+            <input type="text" id="small-input"
+              class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
           </div>
         </div>
+
+        <div date-rangepicker class="flex items-center">
+          <div class="relative">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"></path>
+              </svg>
+            </div>
+            <input name="start" type="text"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="เลือกวันที่">
+          </div>
+          <span class="mx-4 text-gray-500">ถึง</span>
+          <div class="relative">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"></path>
+              </svg>
+            </div>
+            <input name="end" type="text"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="เลือกวันที่">
+          </div>
+        </div>
+
         <div class="mb-6">
           <label for="base-input"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">จำนวนลูกทัวร์</label>
@@ -84,7 +89,12 @@
             class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
         </div>
         <div class="mb-6">
-          <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เบอร์โทร</label>
+          <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เที่ยวบินหรือพาหนะอื่นๆขาไป</label>
+          <input type="text" id="small-input"
+            class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        </div>
+        <div class="mb-6">
+          <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เที่ยวบินหรือพาหนะอื่นๆขากลับ</label>
           <input type="text" id="small-input"
             class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
         </div>
@@ -92,11 +102,11 @@
 
 
       <v-col style="
-              border-radius: 1rem;
-              margin: 1rem;
-              box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
-                rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
-          ">
+                              border-radius: 1rem;
+                              margin: 1rem;
+                              box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
+                                rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
+                          ">
         <h3
           class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white">
           ข้อมูลโรงแรมที่พัก</h3>
@@ -111,8 +121,9 @@
           <input type="text" id="base-input"
             class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
         </div>
-        <div class="flex">
-          <div class="relative max-w-sm">
+
+        <div date-rangepicker class="flex items-center">
+          <div class="relative">
             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -121,11 +132,12 @@
                   clip-rule="evenodd"></path>
               </svg>
             </div>
-            <input datepicker type="text"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="วันที่ไป" />
+            <input name="start" type="text"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="วันที่เช็ดอินน์">
           </div>
-          <div class="relative max-w-sm">
+          <span class="mx-4 text-gray-500">ถึง</span>
+          <div class="relative">
             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -134,9 +146,9 @@
                   clip-rule="evenodd"></path>
               </svg>
             </div>
-            <input datepicker type="text"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="วันที่กลับ" />
+            <input name="end" type="text"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="วันที่เช็ดเอ้าท์">
           </div>
         </div>
 
@@ -167,10 +179,19 @@
         </v-table>
       </v-col>
     </v-row>
-    
+
     <div style="display: flex; justify-content: center;">
       <button type="button" style="margin: 1rem;"
         class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">เพิ่มข้อมูล</button>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+// import Datepicker from 'flowbite-datepicker/Datepicker';
+
+// const datepickerEl = document.getElementById('datepickerId');
+// new Datepicker(datepickerEl, {
+//     // options
+// }); 
+</script>
